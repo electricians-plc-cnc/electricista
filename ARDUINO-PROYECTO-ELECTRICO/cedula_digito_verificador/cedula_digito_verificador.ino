cc2a036e122dc79236064cad2e032219e1c1b7bf
María Eugenia Szwedowski <me.webs.design@gmail.com>
2020-12-21 19:40:55 -0300

    Cédula de Identidad Uruguaya, dígito verificado

--- ARDUINO-PROYECTO-ELECTRICO/cedula_digito_verificador/cedula_digito_verificador.ino
+++ ARDUINO-PROYECTO-ELECTRICO/cedula_digito_verificador/cedula_digito_verificador.ino
@@ -13,19 +13,11 @@ int resultado;
 int suma;
 int dato;
 
-//ARGENTINA
-int dni[10] = {1,2,3,4,5,6,7,8,1,1};
-int algoritmoDni[10] = {5,4,3,2,7,6,5,4,3,2};
-int valoresDni[10];
-int resultadoDni;
-int sumaDni;
-int datoDni;
-
 
 void setup() { 
   Serial.begin(9600); // Abre puerto serie con la velocidad de 9600 baudios (bit por segundo) 
 
-  Argentina();
+  Uruguay();
 }
 
 
@@ -51,32 +43,6 @@ void Uruguay(){
     dato = 0;
     }
     Serial.println(dato);
-    
-    
-}
-
-void Argentina(){
-
-    for(int i=0; i<10; i++){
-      resultadoDni= dni[i]*algoritmoDni[i];
-      valoresDni[i]=resultadoDni;
-      Serial.println(dni[i]);
-
-    }
-    for(int x=0; x<7; x++){
-      sumaDni=sumaDni+valoresDni[x];
-    }
-    
-    int sumaDniResult = sumaDni/11;
-    datoDni = 10-(sumaDniResult%10);
-    int valorOnce = 10-datoDni;
-    
-    if(valorOnce==10){
-    datoDni = 9;
-    }if(valorOnce==11){
-    datoDni = 0;
-    }
-    Serial.println(datoDni);
     
     
 }
